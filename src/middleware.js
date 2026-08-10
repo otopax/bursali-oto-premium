8282ea509b992ecfd090f646b5483917552e2ce6
Bursali Oto (Cowork) <xborsafethiye@gmail.com>
2026-08-10 14:03:12 +0300

    fix(csp): add challenges.cloudflare.com to script-src for Turnstile; gitignore test artifacts

--- src/middleware.js
+++ src/middleware.js
@@ -269,7 +269,7 @@ export async function middleware(request) {
 
     const csp = `
       default-src 'self';
-      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://challenges.cloudflare.com;
       style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
       img-src 'self' blob: data: https:;
       font-src 'self' data: https://fonts.gstatic.com;
